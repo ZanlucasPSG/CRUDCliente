2d71fa9a5bd2ba8ac8a99391f2901c25253340c2
Édini Marques Zanlucas <ezanlucas@PSG.LOCAL>
2021-07-20 13:45:11 -0400

    tentativa de arrumar bugs

--- completa/src/main/java/com/example/demo/rest/ClienteController.java
+++ completa/src/main/java/com/example/demo/rest/ClienteController.java
@@ -4,8 +4,10 @@ import com.example.demo.entity.Cliente;
 import com.example.demo.repository.ClientesRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +22,7 @@ public class ClienteController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(Cliente cliente){
         return repository.save(cliente);
     }
