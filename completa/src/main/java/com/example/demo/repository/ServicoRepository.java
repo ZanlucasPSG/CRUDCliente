9ea9f0b7ecee44e1ad5f84c803b5f612dea93a3c
Édini Marques Zanlucas <ezanlucas@PSG.LOCAL>
2021-07-21 09:32:16 -0400

    remocao do projeto anterior e criacao de nova versao

--- completa/src/main/java/com/example/demo/repository/ServicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Servico;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository <Servico,Integer> {
-    
-}
