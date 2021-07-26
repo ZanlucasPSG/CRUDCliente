03b52439e4de302905d79885cbb58824f13444ba
Édini Marques Zanlucas <ezanlucas@PSG.LOCAL>
2021-07-26 16:08:55 -0400

    WIP tentativa de resolver o entity manager, com uma importacao dessa factory

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.entity.Cliente;
-import com.example.demo.repository.ClienteRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import javax.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import com.example.demo.entity.Cliente;
+import com.example.demo.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class DemoApplication {
